582d2e99911688650a1e080c886cd77e79b20e43
kirkby <kirkby@neighbor.com>
2021-01-26 20:44:00 -0700

    progress!!

--- review/src/App.js
+++ review/src/App.js
@@ -1,15 +1,53 @@
 import React, { useReducer } from "react";
-import reducer from './reducers/calcReducer';
+import reducer, { ADD, SUBTRACT, CLEAR, ADD_TO_MEMORY } from './reducers/calcReducer';
 import "./styles.css";
 
+const initialState = {
+  currentValue: 0,
+  memory: 0
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  //const [currentValue, setCurrentValue] = useState(0)
+
+  const handleAddTwo = e => {
+    e.preventDefault();
+    // do somethign to add 2
+    // setCurrentValue(currentValue + 2)
+    dispatch({ type: ADD, payload: 2 })
+  }
+
+  const handleSubtractThree = e => {
+    e.preventDefault();
+    dispatch({ type: SUBTRACT, payload: 3000000 });
+  }
+
+  const handleClear = e => {
+    e.preventDefault();
+    dispatch({ type: CLEAR })
+  }
+
+  const handleAddToMemory = e => {
+    e.preventDefault()
+    dispatch({ type: ADD_TO_MEMORY })
+  }
+
   return (
     <div className="App">
-      <textarea rows="1" value="0" id="total" type="text" name="ans"></textarea>
+      <textarea rows="1" value={state.currentValue} id="total" type="text" name="ans"></textarea>
       <br />
-      <button type="button" className="btn">
-        +
+      <button type="button" className="btn" onClick={handleAddTwo}>
+        +2
+      </button>
+      <button type="button" className="btn" onClick={handleSubtractThree}>
+        -300000
+      </button>
+      <button type="button" className="btn" onClick={handleClear}>
+        CR
+      </button>
+      <button type="button" className="btn" onClick={handleAddToMemory}>
+        MR
       </button>
     </div>
   );
--- review/src/reducers/calcReducer.js
+++ review/src/reducers/calcReducer.js
@@ -1,17 +1,30 @@
+/*
+   reducer is a function that takes two variables,
+   the state and an action, and returns a modified state
+   based on the action
+
+    action is an object that has a required 'type' key
+    and an optional 'payload'
+*/
+
+export const ADD = 'ADD'
+export const SUBTRACT = 'SUBTRACT'
+export const CLEAR = 'CLEAR'
+export const ADD_TO_MEMORY = 'ADD_TO_MEMORY'
+
 const reducer = (state, action) => {
-    switch (action.type) {
-      case "ADD":
-        return { ...state, currentValue: state.currentValue + action.payload };
-      case "SUBTRACT":
-        return { ...state, currentValue: state.currentValue - action.payload };
-      case "CLEAR":
-        return { ...state, currentValue: 0 };
-      case "ADD_TO_MEMORY":
-        return { ...state, memory: state.currentValue };
-      default:
-        return state;
-    }
-  };
-  
-  export default reducer;
-  
+  switch (action.type) {
+    case ADD:
+      return { ...state, currentValue: state.currentValue + action.payload };
+    case SUBTRACT:
+      return { ...state, currentValue: state.currentValue - action.payload };
+    case CLEAR:
+      return { ...state, currentValue: 0 };
+    case ADD_TO_MEMORY:
+      return { ...state, memory: state.currentValue };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
